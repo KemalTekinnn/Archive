c61e774a1ed59ad92b69fd495b149c7f3e152f8b
Kemal Tekin <markus.kml@gmail.com>
2021-10-25 22:52:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 console.log("Connected");
-
+// Variables
 const modeBtn = document.getElementById("mode");
 const bodyClass = document.getElementById("body-section").classList;
 const h1 = document.querySelector(".main-title").classList;
 const fotLink = document.getElementById("footer-link").classList;
 const fotLink1 = document.getElementById("footer-link1").classList;
 console.log(bodyClass);
-
+// Button Function
 modeBtn.addEventListener("click", function () {
   modeBtn.classList.toggle("white");
   bodyClass.toggle("black");
